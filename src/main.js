f235c64e6395019cabd6174baa973109eed476e9
Hailie17 <192151452@qq.com>
2024-04-06 00:57:50 +0800

    样式穿透

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
 import '@/assets/styles/element-variables.scss'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="login-page">登录</div>
+  <div class="login-page">
+    <el-card>
+      <template #header> 面经后台管理系统 </template>
+      登录框
+    </el-card>
+  </div>
 </template>
 
 <script setup></script>
@@ -8,5 +13,17 @@
 .login-page {
   height: 100vh;
   background: url(@/assets/login-bg.svg) no-repeat center / cover;
+  .el-card {
+    width: 420px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    ::v-deep .el-card__header {
+      text-align: center;
+      background-color: #727cf5;
+      color: #fff;
+    }
+  }
 }
 </style>
